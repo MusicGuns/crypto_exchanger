0e266c2e282d301822234f42beab6c8782ae3f07
Your Name <you@example.com>
2023-03-26 18:44:09 +0000

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  KEY = Bitcoin::Key.from_base58(ENV['PRIVATE_KEY'])
+  KEY = Bitcoin::Key.from_base58('cV1sRTbMDB5757StFD1PibUTxHZPAFEjrNFLSm6BeWAKA6heP2md')
 end
